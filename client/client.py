5dd856f1e640e2fa58691dc87360bae418ab0a9c
kincho4 <75807467+kincho4@users.noreply.github.com>
2022-08-09 12:57:04 +0200

    initial commit

---
+++ client/client.py
@@ -0,0 +1,79 @@
+from pynput.keyboard import *
+import socket
+import os
+import json
+
+print("Welcome to the Robot client!\n")
+while True:
+    empcheck = open("data.json", "r")
+    dod = json.loads(empcheck.read())
+    empcheck.close()
+    if dod["ip"] == "":
+        rpi_ip = input("Enter the local ip of the Robot: ")
+        response = os.system(f"ping -n 1 {rpi_ip}")
+        if response == 0:
+            SERVER = rpi_ip
+            ask_save = input("Would you like to save this in a data file? [y/n]")
+            if ask_save == "y":
+                with open("data.json", "w") as add_ip:
+                    data = {"ip":rpi_ip}
+                    jdata = json.dumps(data)
+                    add_ip.write(jdata)
+                print("Ip written to [data.json] file")
+                break
+            elif ask_save == "n":
+                print("Ok!")
+                break
+            else:
+                print("Thats not a valid option. Please enter: [y/n]")
+        else:
+            print("The IP that you entered is not valid!")
+    else:
+        print("Ip already detected in [data.json]. Using that ip to connect.")
+        break
+
+HEADER = 64
+PORT = 5050
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+print("\n***Press 'h' to view the controls!***")
+
+def motor_move(direction):
+    message = direction.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b" " * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+def press_on(key):
+    if hasattr(key, "char"):
+        if key.char == ("h"):
+            print("\n**Controls**:\nh - Shows this menu\nw - Moves robot forwards\ns - Moves robot backwards\na - Turns the robot to the left\nd - Turns the robot to the right\nl - Low speed\nm - Medium speed\nesc - quits this program\n")
+        elif key.char == ("w"):
+            motor_move("w")
+        elif key.char == ("s"):
+            motor_move("s")
+        elif key.char == ("a"):
+            motor_move("a")
+        elif key.char == ("d"):
+            motor_move("d")
+        else:
+            pass
+    else:
+        pass
+
+
+def press_off(key):
+    if key == Key.esc:
+        motor_move(DISCONNECT_MESSAGE)
+        return False
+
+
+with Listener(on_press=press_on, on_release=press_off) as listener:
+    listener.join()
---
+++ rpi/rpiserver.py
@@ -0,0 +1,129 @@
+import RPi.GPIO as gp
+import socket
+import threading
+
+in1 = 24
+in2 = 23
+enA = 25
+dir1 = 1
+
+in3 = 17
+in4 = 27
+enB = 22
+dir2 = 1
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(enA,GPIO.OUT)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(enB,GPIO.OUT)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
+
+pA=GPIO.PWM(enA,1000)
+pB=GPIO.PWM(enB,1000)
+
+pA.start(25)
+pB.start(25)
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+print(SERVER)
+def handle_client(conn, addr):
+    print(f"[SERVER]: NEW CONNECTON - {addr} -")
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+
+            if msg == "w":
+                print("w")
+                GPIO.output(in1,GPIO.HIGH)
+                GPIO.output(in2,GPIO.LOW)
+                GPIO.output(in3,GPIO.HIGH)
+                GPIO.output(in4,GPIO.LOW)
+
+                GPIO.output(in1,GPIO.LOW)
+                GPIO.output(in2,GPIO.LOW)
+                GPIO.output(in3,GPIO.LOW)
+                GPIO.output(in4,GPIO.LOW)
+
+            elif msg == "s":
+                print("s")
+                GPIO.output(in1,GPIO.LOW)
+                GPIO.output(in2,GPIO.HIGH)
+                GPIO.output(in3,GPIO.LOW)
+                GPIO.output(in4,GPIO.HIGH)
+
+                GPIO.output(in1,GPIO.LOW)
+                GPIO.output(in2,GPIO.LOW)
+                GPIO.output(in3,GPIO.LOW)
+                GPIO.output(in4,GPIO.LOW)
+
+            elif msg == "a":
+                print("a")
+                GPIO.output(in1,GPIO.LOW)
+                GPIO.output(in2,GPIO.LOW)
+                GPIO.output(in3,GPIO.HIGH)
+                GPIO.output(in4,GPIO.LOW)
+
+                GPIO.output(in1,GPIO.LOW)
+                GPIO.output(in2,GPIO.LOW)
+                GPIO.output(in3,GPIO.LOW)
+                GPIO.output(in4,GPIO.LOW)
+
+            elif msg == "d":
+                print("d")
+                GPIO.output(in1,GPIO.HIGH)
+                GPIO.output(in2,GPIO.LOW)
+                GPIO.output(in3,GPIO.LOW)
+                GPIO.output(in4,GPIO.LOW)
+
+                GPIO.output(in1,GPIO.LOW)
+                GPIO.output(in2,GPIO.LOW)
+                GPIO.output(in3,GPIO.LOW)
+                GPIO.output(in4,GPIO.LOW)
+
+            elif msg == "l":
+                print("l")
+                pA.ChangeDutyCycle(25)
+                pB.ChangeDutyCycle(25)
+            elif msg == "m":
+                print("m")
+                pA.ChangeDutyCycle(50)
+                pB.ChangeDutyCycle(50)
+            elif msg == "h":
+                print("h")
+                pA.ChangeDutyCycle(75)
+                pB.ChangeDutyCycle(75)
+
+            else:
+                pass
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+                print(f"[CLIENT]: {addr} - DISCONNECTED -")
+def start():
+    server.listen()
+    print(f"[SERVER]: Server IS RUNNING ON - {SERVER} -")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+
+print("[SERVER]: Server is starting!")
+start()
